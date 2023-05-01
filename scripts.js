39b117f8f7eaa75752572646cae9b6bad699e17b
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-05-01 16:44:45 +0100

    Add a function to add books

--- scripts.js
+++ scripts.js
@@ -3,3 +3,14 @@ const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
 const listOfBooks = document.getElementById('books-list');
 let listArray = JSON.parse(localStorage.getItem('listArray')) || [];
+
+function addBook() {
+  const newBook = {
+    titleOfBook: bookTitle.value,
+    authorOfBook: bookAuthor.value,
+    bookId: Math.floor(Math.random() * 1000000),
+  };
+  listArray.push(newBook);
+  localStorage.setItem('listArray', JSON.stringify(listArray));
+  return true; // Return true to indicate book was added successfully
+}
